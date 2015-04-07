74f79fc51eaeb21cf896f947194b6306c0aaf930
Daniel Chen <dnlchen@ucdavis.edu>
2015-04-07 15:55:55 -0700

    fixing csrf error

--- rcaphone/settings.py
+++ rcaphone/settings.py
@@ -91,10 +91,16 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ]
 }
+
+REST_SESSION_LOGIN = False
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
