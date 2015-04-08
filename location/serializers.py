17c21af3344e127e0ecf86c98d61e0bba136f27c
Daniel Chen <dnlchen@ucdavis.edu>
2015-04-08 14:26:32 -0700

    added REST put function for phones

--- location/serializers.py
+++ location/serializers.py
@@ -1,5 +1,6 @@
 from location.models import Phone, Center
 from rest_framework import serializers
+from django.utils import timezone
 
 
 class CenterSerializer(serializers.ModelSerializer):
@@ -14,3 +15,18 @@ class PhoneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Phone
         fields = ('phone_name', 'updated_time', 'center_set')
+
+    def update(self, instance, validated_data):
+        instance.phone_name = validated_data.get('phone_name', instance.phone_name)
+        instance.updated_time = timezone.now()
+        centers = validated_data.get('center_set', instance.center_set)
+
+        for center in Center.objects.all():
+        	for passedCenter in centers:
+        		if (center.center_name == passedCenter.get('center_name')):
+        			center.current_location = passedCenter.get('current_location')
+        			center.save()
+        			break
+
+        instance.save()
+        return instance
